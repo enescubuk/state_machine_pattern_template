555d00641f57fecc5421f8bc86cca54e6351a970
Enes <70627939+enescubuk@users.noreply.github.com>
2023-12-20 15:34:18 +0200

    idle

---
+++ IdleState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IStateCommand
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Tick()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
